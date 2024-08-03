c2ed77150858b548e0aa910904f3a1c92ca25b58
ravikumar8292 <ravi.kumarrfj@gmail.com>
2024-08-03 18:16:22 +0530

    home page completed

--- frontend/src/components/Home/Banner2.js
+++ frontend/src/components/Home/Banner2.js
@@ -1,13 +1,51 @@
 import React from "react";
 import qrhome from "../../image/scan-qr-code.webp";
+import { motion } from "framer-motion";
+
+const content = {
+  hidden: { opacity: 0, x: "-10vw" },
+  show: {
+    opacity: 1,
+    x: "0",
+    transition: {
+      type: "spring",
+      delay: 0.2,
+    },
+  },
+};
+
+const picContainer = {
+  hidden: { opacity: 0, x: "10vw" },
+  show: {
+    opacity: 1,
+    x: "0",
+    transition: {
+      type: "spring",
+      delay: 0.2,
+    },
+  },
+};
 
 const Banner2 = () => {
   return (
     <div className="container mx-auto pl-14 py-[90px] flex gap-2">
-      <div className="ml-12">
+      <motion.div
+        variants={content}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="ml-12"
+      >
         <img src={qrhome} alt="" srcset="" className="w-[1500px] h-[450px]" />
-      </div>
-      <div className="mt-[130px] px-14 ml-[90px]">
+      </motion.div>
+
+      <motion.div
+        variants={picContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="mt-[130px] px-14 ml-[90px]"
+      >
         <h2 className="text-4xl font-bold">
           How do I scan the QR code in the image?
         </h2>
@@ -22,7 +60,7 @@ const Banner2 = () => {
           in your device to fetch the product details correctly in a convenient
           way.
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 };
--- frontend/src/components/Home/Banner3.js
+++ frontend/src/components/Home/Banner3.js
@@ -1,10 +1,41 @@
 import React from "react";
 import qrhome from "../../image/why-business-need-qr-code-scanner.webp";
+import { motion } from "framer-motion";
+
+const content = {
+  hidden: { opacity: 0, x: "-10vw" },
+  show: {
+    opacity: 1,
+    x: "0",
+    transition: {
+      type: "spring",
+      delay: 0.2,
+    },
+  },
+};
+
+const picContainer = {
+  hidden: { opacity: 0, x: "10vw" },
+  show: {
+    opacity: 1,
+    x: "0",
+    transition: {
+      type: "spring",
+      delay: 0.2,
+    },
+  },
+};
 
 const Banner3 = () => {
   return (
     <div className="container mx-auto pr-14 py-[90px] flex gap-2">
-      <div className="mt-[130px] pl-10 mr-14 ml-[90px] border">
+      <motion.div
+        variants={content}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="mt-[130px] pl-10 mr-14 ml-[90px] border"
+      >
         <h2 className="text-4xl font-bold">
           Why do businesses use QR code image to fetch product/payment details?
         </h2>
@@ -20,10 +51,17 @@ const Banner3 = () => {
           helps you add a QR code within the invoice, and your customer can scan
           it within the device itself to pay you.
         </p>
-      </div>
-      <div className="">
+        </motion.div>
+
+      <motion.div
+        variants={picContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="w-30vw h-30vh"
+      >
         <img src={qrhome} alt="" srcset="" className="w-[1900px] h-[450px]" />
-      </div>
+      </motion.div>
     </div>
   );
 };
